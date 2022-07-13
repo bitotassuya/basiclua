2dfd78d7826b7e228e80e1f34ef91bc1b73c9f9d
bitotassuya <jareansong@gmail.com>
2022-07-13 20:22:53 +0700

    Frist Time

---
+++ Basic1.lua
@@ -0,0 +1,15 @@
+number = 10
+print(number)
+
+
+
+-- 1 line comment
+
+
+--[[ multi line comment
+    multi line comment
+    multi line comment
+    .
+    .
+    .
+--]]
---
+++ DataTypes.lua
@@ -0,0 +1,15 @@
+print(type("Hello world")) -- string
+print(type(10.4 * 3)) -- number
+print(type(print)) -- function
+print(type(type)) -- function
+print(type(true)) -- boolean
+print(type(nil)) -- nil
+print(type(type(X))) -- string
+
+print(A)
+
+A = true
+B = false
+if A == true then
+    print("A =", true)
+end
