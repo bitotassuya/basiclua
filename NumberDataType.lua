b7681c8111405e5ab693d55b1704aecf1a892b7e
bitotassuya <jareansong@gmail.com>
2022-07-14 20:44:28 +0700

    StringTypeData

---
+++ NumberDataType.lua
@@ -0,0 +1,14 @@
+PI = 22 / 7
+NumberOne = 3.123456789
+NumberSecond = 2
+print(PI)
+print(string.format("%.18f", PI))
+print("%.18f", PI)
+print(string.format("%ef", NumberOne))
+print(NumberSecond)
+print(type(2))
+print(type(2.2))
+print(type(0.2))
+print(type(2e+1))
+print(type(0.2e-1))
+print(type(7.8263692594256e-06))
---
+++ StringDataType.lua
@@ -0,0 +1,21 @@
+NickName = 'Non'
+SurName = "Jirapong"
+print("My Nick name is =",
+    NickName, "\n", "MySurname=", SurName)
+
+RealName = [[
+Sure name is Jirapong
+Last name is Jareansong
+Nick name is Non
+]]
+print(RealName)
+
+Additionnal = [[
+"8" + "6"
+"8" + 6
+"8 + 6"
+]]
+print(Additionnal)
+print("8" + "6")
+print("8" + 6)
+print("8 + 6")
