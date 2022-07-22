efcac7ab599aba626f0181c980ca005cc862c547
bitotassuya <jareansong@gmail.com>
2022-07-22 22:47:04 +0700

    RelationOperator

---
+++ RelationalOperators.lua
@@ -0,0 +1,26 @@
+FirstNumber = 10
+SecondNumber = 5
+
+if FirstNumber == 10 then
+    print("First Number = 10 is true")
+end
+
+if FirstNumber ~= SecondNumber then
+    print("FirstNumber is not equal Second Number")
+end
+
+if FirstNumber > SecondNumber then
+    print("FirstNumber more than Second Number is true")
+end
+
+if FirstNumber >= SecondNumber then
+    print("FirstNumber more than or equal Second Number is true")
+end
+
+if SecondNumber < FirstNumber then
+    print("Second Number less than FirstNumber  is true")
+end
+
+if SecondNumber <= FirstNumber then
+    print("Second Number less than or equal FirstNumber  is true")
+end
